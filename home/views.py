b1e1da85ca3108e551003c12dedb9307767178e0
Abhiram970 <abhiramp428@gmail.com>
2022-04-29 01:08:43 +0530

    Predictions and other work is done

--- home/views.py
+++ home/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.contrib.auth.models import auth
 # Create your views here.
+from django.http import HttpResponseRedirect
+import time
+
 def home(request):
     return render(request, 'home/index.html')
 
--- predictions/views.py
+++ predictions/views.py
@@ -6,6 +6,8 @@ import pandas as pd
 from datetime import datetime
 import pickle
 import numpy as np
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 
 warnings.filterwarnings("ignore")
 
@@ -28,6 +30,7 @@ def getPredictions(date):
     return final_list
 
 # Create your views here.
+@login_required(login_url='http://127.0.0.1:8000/login/')
 def predict(request):
     if request.method == "POST":
         day1 = request.POST['day']
@@ -38,7 +41,7 @@ def predict(request):
 
         result = getPredictions(final_str)
         print("Predicted Tesla stock closing value: " + str(result[0]))
-        messages.info(request, "Predicted Tesla stock closing value: " + str(result[0]) )
+        messages.info(request, "Predicted Tesla stock closing value : " + str(result[0]) )
         return HttpResponseRedirect(request.path_info)
     else:
         return render(request,'prediction/pred.html')
