aa85f40d6ccd30e2439e6808213159b0340d1d27
Abhiram970 <abhiramp428@gmail.com>
2022-05-02 14:02:57 +0530

    Small changes again

--- news/views.py
+++ news/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 import requests
 from django.contrib.auth.decorators import login_required
+from datetime import date
+today = date.today()
 # Create your views here.
 
 @login_required(login_url='http://127.0.0.1:8000/login/')
 def home2(request):
-    url = f'https://gnews.io/api/v4/search?q=example&token=522813761f2a6540308e793ba2ecddfd'
+    url = f'https://newsapi.org/v2/everything?q=business&from={today}&sortBy=publishedAt&lang=en&apiKey=ab33356e87d442da83f47ee4856970a2'
     response = requests.get(url)
     data = response.json()
     articles = data['articles']
@@ -21,11 +23,11 @@ def home2(request):
         article = articles[i]
         desc.append(article['description'])
         news.append(article['title'])
-        img.append(article['image'])
+        img.append(article['urlToImage'])
         date.append(article['publishedAt'])
-        # author.append(article['author'])
+        author.append(article['author'])
         source.append(article['source']['name'])
         url.append(article['url'])
-    mylist = zip(news, desc, img,date,source,url)
+    mylist = zip(news, desc, img,date,author,source,url)
 
     return render(request, "news/news.html", context={"mylist": mylist})
